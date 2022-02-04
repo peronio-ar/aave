92bf01e5ecb2c886ad63da6cc48074a1aff7997b
Agustin Kassis <webmaster@masize.com>
2022-02-04 14:08:37 -0300

    set treasury address

--- deploy/01_deploy_uniswap.js
+++ deploy/01_deploy_uniswap.js
@@ -1,16 +1,25 @@
 // deploy/01_deploy_uniswap.js
 
+const { ethers } = require('hardhat');
+
 module.exports = async ({ deployments, network }) => {
   console.info('Deploying Uniswap');
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const factoryContract = await deploy('UniswapV2Factory', {
+  const factoryDeployed = await deploy('UniswapV2Factory', {
     contract: 'UniswapV2Factory',
     from: deployer,
     log: true,
     args: [deployer],
   });
+
+  const factoryContract = await ethers.getContractAt(
+    factoryDeployed.abi,
+    factoryDeployed.address
+  );
+  console.info('Setting fee receiver to ' + process.env.TREASURY_ADDRESS);
+  (await factoryContract.setFeeTo(process.env.TREASURY_ADDRESS)).wait();
 
   const routerContract = await deploy('UniswapV2Router02', {
     contract: 'UniswapV2Router02',
